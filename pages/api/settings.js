03472e4e52eaac13ecbeef8f26ad66eaf61d6086
realbcole <realbcole@gmail.com>
2023-06-10 15:30:37 -0400

    added stripe, added setting to create coupons

--- pages/api/settings.js
+++ pages/api/settings.js
@@ -1,6 +1,7 @@
 import { mongooseConnect } from '@/lib/mongoose';
 import { isAdminRequest } from './auth/[...nextauth]';
 import { Settings } from '@/models/Settings';
+const stripe = require('stripe')(process.env.STRIPE_SK);
 
 export default async function handler(req, res) {
   await mongooseConnect();
@@ -21,6 +22,95 @@ export default async function handler(req, res) {
     // If not, create
     else {
       settingsDoc = await Settings.create({ name, value });
+    }
+
+    if (name === 'coupons') {
+      const stripeCoupons = await stripe.coupons.list({});
+      const existingCouponNames = stripeCoupons.data.map(
+        (coupon) => coupon.name
+      );
+
+      const couponNamesToCreate = Object.keys(value).filter(
+        (name) => !existingCouponNames.includes(name)
+      );
+      const couponNamesToDelete = existingCouponNames.filter(
+        (name) => !Object.keys(value).includes(name)
+      );
+      const couponNamesToUpdate = Object.keys(value).filter((name) =>
+        existingCouponNames.includes(name)
+      );
+
+      // Create new coupons
+      for (const couponName of couponNamesToCreate) {
+        const couponData = value[couponName];
+        let newCoupon;
+        if (couponData.percentOff) {
+          newCoupon = await stripe.coupons.create({
+            name: couponName,
+            percent_off: couponData.percentOff,
+          });
+        } else if (couponData.amountOff) {
+          newCoupon = await stripe.coupons.create({
+            name: couponName,
+            amount_off: couponData.amountOff * 100,
+            currency: 'usd',
+          });
+        }
+        console.log(`Created coupon ${newCoupon.name}`);
+
+        // Create promotional code
+        const promoCode = await stripe.promotionCodes.create({
+          coupon: newCoupon.id,
+          code: couponName,
+        });
+        console.log(`Created promo code ${promoCode.code}`);
+      }
+
+      // Update existing coupons
+      for (const couponName of couponNamesToUpdate) {
+        const couponData = value[couponName];
+        const existingCoupon = stripeCoupons.data.find(
+          (coupon) => coupon.name === couponName
+        );
+        if (existingCoupon) {
+          // Delete the existing coupon
+          await stripe.coupons.del(existingCoupon.id);
+          console.log(`Deleted coupon ${existingCoupon.name}`);
+          // Create new coupon with updated data
+          let newCoupon;
+          if (couponData.percentOff) {
+            newCoupon = await stripe.coupons.create({
+              name: couponName,
+              percent_off: couponData.percentOff,
+            });
+          } else if (couponData.amountOff) {
+            newCoupon = await stripe.coupons.create({
+              name: couponName,
+              amount_off: couponData.amountOff * 100,
+              currency: 'usd',
+            });
+          }
+          console.log(`Created coupon ${newCoupon.name}`);
+
+          // Create promotional code
+          const promoCode = await stripe.promotionCodes.create({
+            coupon: newCoupon.id,
+            code: couponName,
+          });
+          console.log(`Created promotional code ${promoCode.id}`);
+        }
+      }
+
+      // Delete coupons
+      for (const couponName of couponNamesToDelete) {
+        const existingCoupon = stripeCoupons.data.find(
+          (coupon) => coupon.name === couponName
+        );
+        if (existingCoupon) {
+          await stripe.coupons.del(existingCoupon.id);
+          console.log(`Deleted coupon ${existingCoupon.name}`);
+        }
+      }
     }
 
     res.json(settingsDoc);
--- pages/settings.js
+++ pages/settings.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { withSwal } from 'react-sweetalert2';
 import Layout from '@/components/Layout';
 import Spinner from '@/components/Spinner';
+import DeleteIcon from '@/components/icons/DeleteIcon';
 
 // Settings Page
 const SettingsPage = ({ swal }) => {
@@ -10,6 +11,8 @@ const SettingsPage = ({ swal }) => {
   const [featuredProduct, setFeaturedProduct] = useState('');
   const [shippingFee, setShippingFee] = useState(0);
   const [shopName, setShopName] = useState('');
+  const [coupons, setCoupons] = useState({});
+  const [couponTypes, setCouponTypes] = useState({});
   const [isLoading, setIsLoading] = useState(false);
 
   // On start, load settings
@@ -28,10 +31,40 @@ const SettingsPage = ({ swal }) => {
     await axios.get(`/api/settings?name=shippingFee`).then((response) => {
       setShippingFee(response?.data?.value);
     });
+    await axios.get(`/api/settings?name=shopName`).then((response) => {
+      setShopName(response?.data?.value);
+    });
+    await axios.get(`/api/settings?name=coupons`).then((response) => {
+      const coupons = response?.data?.value;
+      setCoupons(coupons);
+      const couponTypes = {};
+      for (const coupon in coupons) {
+        if (coupons.hasOwnProperty(coupon)) {
+          const type = Object.keys(coupons[coupon])[0];
+          couponTypes[coupon] = type;
+        }
+      }
+      setCouponTypes(couponTypes);
+    });
   }
 
   async function saveSettings() {
     setIsLoading(true);
+    const processedCoupons = Object.keys(coupons).reduce((acc, couponName) => {
+      const couponType = couponTypes[couponName];
+      acc[couponName] = {
+        [couponType]: coupons[couponName][couponType],
+      };
+
+      // Add currency when coupon type is amountOff
+      if (couponType === 'amountOff') {
+        acc[couponName].currency = 'usd';
+      }
+
+      return acc;
+    }, {});
+
+    console.log({ processedCoupons });
     await axios.put('/api/settings', {
       name: 'featuredProductId',
       value: featuredProduct,
@@ -44,6 +77,10 @@ const SettingsPage = ({ swal }) => {
       name: 'shopName',
       value: shopName,
     });
+    await axios.put('/api/settings', {
+      name: 'coupons',
+      value: processedCoupons,
+    });
     setIsLoading(false);
     await swal.fire({
       title: 'Settings saved!',
@@ -51,6 +88,58 @@ const SettingsPage = ({ swal }) => {
       icon: 'success',
       iconColor: '#293241',
       background: '#E0FBFC',
+    });
+  }
+
+  function addCoupon() {
+    setCoupons((prev) => {
+      let newCoupon = `newCoupon${Date.now()}`;
+      return { ...prev, [newCoupon]: {} };
+    });
+    setCouponTypes((prev) => {
+      let newCoupon = `newCoupon${Object.keys(prev)?.length || 1}`;
+      return { ...prev, [newCoupon]: '' };
+    });
+  }
+
+  function handleCouponChange(couponName, newValues) {
+    setCoupons((prev) => {
+      let updatedCoupon = { ...prev[couponName], ...newValues };
+      return { ...prev, [couponName]: updatedCoupon };
+    });
+  }
+
+  function handleCouponNameChange(oldName, newName) {
+    setCoupons((prev) => {
+      // Create a new object to avoid mutating the state directly
+      const newCoupons = { ...prev };
+
+      // Create new coupon with new name
+      newCoupons[newName] = newCoupons[oldName];
+
+      // Delete old coupon
+      delete newCoupons[oldName];
+
+      return newCoupons;
+    });
+    setCouponTypes((prev) => {
+      const newCouponTypes = { ...prev };
+      newCouponTypes[newName] = newCouponTypes[oldName];
+      delete newCouponTypes[oldName];
+      return newCouponTypes;
+    });
+  }
+
+  function removeCoupon(couponName) {
+    setCoupons((prev) => {
+      const coupons = { ...prev };
+      delete coupons[couponName];
+      return coupons;
+    });
+    setCouponTypes((prev) => {
+      const newCouponTypes = { ...prev };
+      delete newCouponTypes[couponName];
+      return newCouponTypes;
     });
   }
 
@@ -88,8 +177,99 @@ const SettingsPage = ({ swal }) => {
             value={shippingFee}
             onChange={(e) => setShippingFee(e.target.value)}
           />
+          {/* Coupons */}
+          {/* TODO: Add option to only apply coupon to certain categories or products */}
+          <div className="">
+            <label>Coupons</label>
+            <button className="btn-default block" onClick={addCoupon}>
+              Add Coupon
+            </button>
+            {coupons &&
+              typeof coupons === 'object' &&
+              Object.keys(coupons).map((couponName, index) => (
+                <div key={index}>
+                  <h2 className="uppercase flex items-center text-xl mb-2">
+                    New Coupon
+                    <div className="py-2 px-1">
+                      <button
+                        onClick={() => removeCoupon(couponName)}
+                        className="text-secondaryBg flex items-center text-sm bg-secondary rounded-full py-1 px-2"
+                      >
+                        <DeleteIcon /> Delete
+                      </button>
+                    </div>
+                  </h2>
+                  <div className="flex gap-2">
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      value={couponName.toUpperCase()}
+                      onChange={(e) =>
+                        handleCouponNameChange(
+                          couponName,
+                          e.target.value.toUpperCase()
+                        )
+                      }
+                    />
+                  </div>
+                  <div className="flex gap-2">
+                    <label className="whitespace-nowrap">Coupon Type</label>
+                    <select
+                      value={couponTypes[couponName] || ''}
+                      onChange={(e) => {
+                        setCouponTypes((prev) => {
+                          const newCouponTypes = { ...prev };
+                          newCouponTypes[couponName] = e.target.value;
+                          return newCouponTypes;
+                        });
+                      }}
+                    >
+                      <option value="">Select Coupon Type</option>
+                      <option value="percentOff">Percent Off</option>
+                      <option value="amountOff">Amount Off</option>
+                    </select>
+                  </div>
+                  {couponTypes[couponName] && couponTypes[couponName] != '' && (
+                    <>
+                      {console.log(coupons[couponName])}
+                      {couponTypes[couponName] === 'amountOff' ? (
+                        <div className="flex gap-2">
+                          <label className="whitespace-nowrap">
+                            Amount Off (USD)
+                          </label>
+                          <input
+                            type="number"
+                            value={coupons[couponName].amountOff}
+                            onChange={(e) =>
+                              handleCouponChange(couponName, {
+                                amountOff: e.target.value,
+                              })
+                            }
+                          />
+                        </div>
+                      ) : (
+                        <div className="flex gap-2">
+                          <label className="whitespace-nowrap">
+                            Percent Off (%)
+                          </label>
+                          <input
+                            type="number"
+                            value={coupons[couponName].percentOff}
+                            onChange={(e) =>
+                              handleCouponChange(couponName, {
+                                percentOff: e.target.value,
+                              })
+                            }
+                          />
+                        </div>
+                      )}
+                    </>
+                  )}
+                </div>
+              ))}
+          </div>
           {/* Save button */}
-          <button className="btn-primary" onClick={saveSettings}>
+          <button className="btn-primary mt-2" onClick={saveSettings}>
             Save
           </button>
         </>
